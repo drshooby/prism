09efb7436fd330fb830a1206d187ed94d505141f
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-10-24 23:23:28 -0700

    add: get infisical go sdk working w/ docs

--- go-service/main.go
+++ go-service/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
+	infisical "github.com/infisical/go-sdk"
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -41,6 +44,34 @@ func main() {
 			return c.String(http.StatusInternalServerError, "❌ DB not reachable")
 		}
 		return c.String(http.StatusOK, "✅ DB connection OK")
+	})
+
+	client := infisical.NewInfisicalClient(context.Background(), infisical.Config{
+		SiteUrl: "http://infisical-backend:8080",
+	})
+
+	// For machine identity (what go sdk uses)
+	// 1. Org -> Access Control -> Identities -> Create Identity w/ Member Role
+	// 2. Secrets Manager -> Access Management -> Machine Identities -> Add Identity -> Select w/ Developer Role
+	// 3. Org -> Access Control -> Identities -> Click Identity -> Universal Auth
+	// -> Copy Client ID -> Create Client Secret -> Copy Client Secret
+
+	_, err = client.Auth().UniversalAuthLogin("0149ab3c-ddc4-4487-aef4-b052ddf809f6", "8920d6201a6ca6d6ad21bba940f825f1f6d2aafb8ebd5af8b97c05b26dde1098")
+	if err != nil {
+		panic(fmt.Sprintf("Authentication failed: %v", err))
+	}
+
+	e.GET("/secrets", func(c echo.Context) error {
+		testSecret, err := client.Secrets().Retrieve(infisical.RetrieveSecretOptions{
+			SecretKey:   "testSecret",
+			Environment: "dev",
+			ProjectID:   "dec7cfaf-8b50-48b4-8577-12035f9dd954", // project settings -> copy project id
+			SecretPath:  "/",
+		})
+		if err != nil {
+			return c.String(200, fmt.Sprintf("Error: %v", err))
+		}
+		return c.String(200, fmt.Sprintf("Your secret: %s", testSecret.SecretValue))
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
