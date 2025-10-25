4bf0178f09313d163beb2c924b3668c54572cba9
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-10-25 14:44:33 -0700

    add: refactor and all secrets endpoint

---
+++ go-service/internal/infisical/client.go
@@ -0,0 +1,85 @@
+package infisical
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	infisical "github.com/infisical/go-sdk"
+)
+
+type InfisicalClientConfig struct {
+	SiteUrl               string
+	InfisicalClientID     string
+	InfisicalClientSecret string
+}
+
+type InfisicalSecretOptions struct {
+	Environment string
+	ProjectID   string
+	SecretPath  string
+}
+
+type InfisicalClient struct {
+	client infisical.InfisicalClientInterface
+}
+
+type GetSecretResponse struct {
+	Secrets    []string `json:"secret"`
+	StatusCode int      `json:"statusCode"`
+	Error      string   `json:"error,omitempty"`
+}
+
+func NewInfisicalClient(config *InfisicalClientConfig) (*InfisicalClient, error) {
+	infisicalClientInterface := infisical.NewInfisicalClient(context.Background(), infisical.Config{
+		SiteUrl:          config.SiteUrl,
+		AutoTokenRefresh: true,
+	})
+
+	if infisicalClientInterface == nil {
+		log.Fatal("infisicalClient is nil")
+	}
+
+	// For machine identity (what go sdk uses)
+	// 1. Org -> Access Control -> Identities -> Create Identity w/ Member Role
+	// 2. Secrets Manager -> Access Management -> Machine Identities -> Add Identity -> Select w/ Developer Role
+	// 3. Org -> Access Control -> Identities -> Click Identity -> Universal Auth
+	// -> Copy Client ID -> Create Client Secret -> Copy Client Secret
+	_, err := infisicalClientInterface.Auth().UniversalAuthLogin(config.InfisicalClientID, config.InfisicalClientSecret)
+	if err != nil {
+		panic(fmt.Sprintf("Authentication failed: %v", err))
+	}
+
+	return &InfisicalClient{
+		client: infisicalClientInterface,
+	}, nil
+}
+
+func NewListSecretOptions(base InfisicalSecretOptions) infisical.ListSecretsOptions {
+	return infisical.ListSecretsOptions{
+		Environment:        base.Environment,
+		ProjectID:          base.ProjectID,
+		SecretPath:         base.SecretPath,
+		AttachToProcessEnv: true, // access secrets with getEnv
+	}
+}
+
+func (infisicalClient InfisicalClient) ListSecrets(options *InfisicalSecretOptions) *GetSecretResponse {
+	secrets, err := infisicalClient.client.Secrets().List(NewListSecretOptions(*options))
+	if err != nil {
+		return &GetSecretResponse{
+			StatusCode: 500,
+			Error:      fmt.Sprintf("Error getting secrets: %v", err),
+		}
+	}
+
+	secretValues := make([]string, len(secrets))
+	for i, s := range secrets {
+		secretValues[i] = s.SecretValue
+	}
+
+	return &GetSecretResponse{
+		Secrets:    secretValues,
+		StatusCode: 200,
+	}
+}
--- go-service/routes.go
+++ go-service/routes.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
 
+	"github.com/benkamin03/prism/internal/infisical"
 	"github.com/benkamin03/prism/internal/minio"
-	infisical "github.com/infisical/go-sdk"
 	"github.com/labstack/echo/v4"
 )
 
 type RoutesConfig struct {
-	Echo           *echo.Echo
-	DatabaseClient *sql.DB
-	InfiClient     infisical.InfisicalClientInterface
-	MinioClient    minio.MinioClient
+	Echo            *echo.Echo
+	DatabaseClient  *sql.DB
+	InfisicalClient infisical.InfisicalClient
+	MinioClient     minio.MinioClient
 }
 
 func SetupRoutes(routesConfig *RoutesConfig) {
@@ -30,21 +29,13 @@ func SetupRoutes(routesConfig *RoutesConfig) {
 		return c.String(http.StatusOK, "✅ DB connection OK")
 	})
 
-	e.GET("/secrets", func(c echo.Context) error {
-		testSecret, err := routesConfig.InfiClient.Secrets().Retrieve(infisical.RetrieveSecretOptions{
-			SecretKey:   "testSecret",
-			Environment: "dev",
-			ProjectID:   "dec7cfaf-8b50-48b4-8577-12035f9dd954", // project settings -> copy project id
-			SecretPath:  "/",
-		})
-		if err != nil {
-			return c.String(200, fmt.Sprintf("Error: %v", err))
-		}
-		return c.String(200, fmt.Sprintf("Your secret: %s", testSecret.SecretValue))
-	})
-
 	minio.SetupRoutes(&minio.MinioRoutesConfig{
 		MinioClient: routesConfig.MinioClient,
 		Echo:        e,
+	})
+
+	infisical.SetupRoutes(&infisical.InfisicalRoutesConfig{
+		InfisicalClient: routesConfig.InfisicalClient,
+		Echo:            e,
 	})
 }
