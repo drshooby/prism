0b315dcd994c5ed07fc2c5569e6996f38047d9b9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-10-25 21:33:12 -0700

    Clean up markup boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,46 +19,16 @@ export default async function Home() {
 
   return (
     <main>
-      <div>
-        <div>
-          {data?.map((repo) => (
-            <div key={repo.id}>{repo.name}</div>
-          ))}
-        </div>
-
-        <h1>
-          Create <span>T3</span> App
-        </h1>
-        <div>
-          <Link
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3>HELLOOOOO</h3>
-            <p>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </p>
-          </Link>
-          <Link href="https://create.t3.gg/en/introduction" target="_blank">
-            <h3>Documentation →</h3>
-            <p>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </p>
-          </Link>
-        </div>
-        <div>
-          <p>{hello ? hello.greeting : "Loading tRPC query..."}</p>
-
-          <div>
-            <p>{session && <span>Logged in as {session.user?.name}</span>}</p>
-            <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
-      </div>
+      <p>{hello ? hello.greeting : "Loading tRPC query..."}</p>
+      <p>{session && <span>Logged in as {session.user?.name}</span>}</p>
+      <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+        {session ? "Sign out" : "Sign in"}
+      </Link>
+      <ul>
+        {data?.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
     </main>
   )
 }
